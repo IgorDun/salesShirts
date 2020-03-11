9f9722f8d95311fd6080b4e037dac23d8aaa2e9e
IgorDun <dunaevigor@mail.ru>
2020-03-12 01:49:03 +0400

    change code

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         int standardPriceShirt = 1200;
-        int amountShirts = 2;
+        int amountShirts = 3;
         int amountShirtsForSales = 4;
         System.out.println("standardPriceShirt = " + standardPriceShirt);
         System.out.println("amountShirts = " + amountShirts);
@@ -23,9 +23,9 @@ public class Main {
         System.out.println("saleSummAmountShirtsForSales = " + saleSummAmountShirtsForSales);
         double saleSummOneShirt = standardPriceShirt * (1 - saleProcent);
         System.out.println("saleSummOneShirt = " + saleSummOneShirt);
-        double savingMoneyForOneShirt = standardPriceShirt - saleSummOneShirt;
-        System.out.println("savingMoneyForOneShirt = " + savingMoneyForOneShirt);
-        double valueOnSales = (summAmountShirtsForSales - saleSummAmountShirtsForSales) / standardPriceShirt;
+        double savingMoneyForShirts = summAmountShirtsForSales - saleSummAmountShirtsForSales;
+        System.out.println("savingMoneyForShirts = " + savingMoneyForShirts);
+        double valueOnSales = savingMoneyForShirts / standardPriceShirt;
         System.out.println("valueOnSales = " + valueOnSales);
     }
 }
